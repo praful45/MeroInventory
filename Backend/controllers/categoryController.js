5a3d00b11ad8b15f931f3f31d9962ae8cc12205f
SONGOKU100 <gurungp599@gmail.com>
2023-05-29 20:12:00 +0545

    update and delete category

--- Backend/controllers/categoryController.js
+++ Backend/controllers/categoryController.js
@@ -28,3 +28,43 @@ exports.createCategory = async (req, res) => {
   }
 };
 
+// update category
+
+exports.updateCategory = async (req, res ) => {
+  const categoryId = req.params.id;
+  const { name, description } = req.body;
+
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      { name, description },
+      { new: true }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ error: 'Product category not found' });
+    }
+
+    res.json(updatedCategory);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+// delete category
+exports.deleteCategory = async (req, res) => {
+  const categoryId = req.params.id;
+
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(categoryId);
+
+    if(!deletedCategory){
+      return res.status(404).json({error: 'Category not found'});
+    }
+    res.sendStatus(204);
+  }
+  
+  catch (err) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+} 
--- Backend/routes/categoryRoute.js
+++ Backend/routes/categoryRoute.js
@@ -3,11 +3,18 @@ const router = express.Router();
 
 const categoryController = require('../controllers/categoryController');
 
-// Get all products
+// Get all cateogries
 router.get('/getallcategories', categoryController.getAllCategory);
 
 
-// Create a new product
+// Create a new category
 router.post('/create-category', categoryController.createCategory);
+
+// update category
+router.put('/update-category/:id', categoryController.updateCategory);
+
+// delete category
+router.delete('/delete-category/:id', categoryController.deleteCategory);
+
 
 module.exports = router;
