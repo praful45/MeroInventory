50a5d8cf9aac4e44f4624f2590f0dbd7185c8776
amritrijal <rijalamrit555@gmail.com>
2023-06-02 23:47:26 +0545

    Category count,InvRoutes

--- frontend/src/pages/routes/InvRoutes.js
+++ frontend/src/pages/routes/InvRoutes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom'
 import Category from "../../components/CategoryList";
-import DashBoard from "../AdminDashboard";
+// import DashBoard from "../AdminDashboard";
 import CategoryAdd from "../../components/CategoryAdd";
 import CategoryListEdit from "../../components/CategoryListEdit";
 import CategoryListDelete from "../../components/CategoryListDelete";
-
+import DashBoard from "../../components/Dashboard";
 
 const InvRoutes = () => {
   return (
@@ -16,6 +16,7 @@ const InvRoutes = () => {
         <Route path="/category-add" element={<CategoryAdd />} />
         <Route path="/category-edit" element={<CategoryListEdit />} />
         <Route path="/category-delete" element={<CategoryListDelete />} />
+        <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
     </>
   );
