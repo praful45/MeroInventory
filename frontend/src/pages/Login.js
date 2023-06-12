3226c7eeaf00fd3d807acc9998ecad75e911d13c
bibek131 <lamichhanebibek1234@gmail.com>
2023-06-12 21:50:45 +0545

    completed

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,15 @@
-
-
-
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import loginImage from '.\loginImage.png'
+// import userProfileImage from '.loginImage-userPofile.png';
+import "./Login.css"
+
 
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  // const history = useHistory();
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -44,24 +44,38 @@ const LoginForm = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
+    <BrowserRouter>
+      <div className='signIn'>
+        <div className='signIn-page-image' style={{ width: "50%" }}>
+
+          <img style={{
+            height: "100%",
+            width: "100%",
+            objectFit: "cover"
+          }} src=".\loginImage.png" alt="LoginImage" />
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} />
+        <div className='signIn-form' style={{ width: "50%" }}>
+          <div >
+            <h2 className='login' style={{ textAlign: "center" }}>Sign In</h2>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+               <div>
+
+                <input placeholder='Enter Username' style={{ borderBottom: "5px solid #AFD3E2", borderTop: "none", borderLeft: "none", borderRight: "none", marginBottom: "20px",  backgroundImage:'url(./loginImage-userPofile.png)', backgroundRepeat: 'no-repeat', backgroundSize:'20px' }} type="text" value={username} onChange={handleUsernameChange} />
+              </div>
+              <div> 
+                <input className='inputUserName' placeholder='Enter Password' style={{ borderBottom: "5px solid #AFD3E2", borderTop: "none", borderLeft: "none", borderRight: "none", marginBottom: "20px", backgroundImage:'url(./loginImage-userPassword.png)', backgroundRepeat: 'no-repeat', backgroundSize:'25px' }} type="password" value={password} onChange={handlePasswordChange}  /> 
+              </div>
+              <div style={{ position: "relative", left: "54px", fontWeight: "bold", cursor: 'pointer',fontSize: '13px' }} >Forget Password ?</div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
         </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+
+      </div>
+
+    </BrowserRouter>
   );
 };
 
 export default LoginForm;
-
-
